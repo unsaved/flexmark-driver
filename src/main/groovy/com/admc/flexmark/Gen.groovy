90073f494529faab7f775f11a883b3329f323195
Blaine Simpson <blaine.simpson@admc.com>
2017-03-05 15:05:07 -0500

    Original Git project population

---
+++ src/main/groovy/com/admc/flexmark/Gen.groovy
@@ -0,0 +1,128 @@
+package com.admc.flexmark
+
+import com.vladsch.flexmark.samples.BasicSample
+import groovy.text.SimpleTemplateEngine
+
+class Gen {
+    final static String DEFAULT_BPTOP_RES_PATH =
+      'boilerplates/basic-top.html'
+    final static String DEFAULT_BPBOTTOM_RES_PATH =
+      'boilerplates/basic-bottom.html'
+
+    private def boilerplatePre  // Class it properly when fucking API is online!
+    private String boilerplatePost
+
+    static private String sampleMd =
+'''[TOC]
+## Header lvl 2
+Testing line breaks with line one
+and line two with a http://cnn.com URL link,
+and line three
+
+A second paragraph with line onw
+and line two
+
+An here is a para  
+With two explicit  
+line breaks.  These suck since can't see them in the MD file.
+
+
+```css
+body {
+    margin: 0;
+    padding: 0;
+    /* A CSS comment is here
+    extending down to there */
+}
+```
+
+### A table
+Dimensions | Megapixels
+---|---
+1,920 x 1,080 | 2.1MP
+3,264 x 2,448 | 8MP
+4,288 x 3,216 | 14MP
+
+'''
+
+    private Map<String,String> substrMap = [
+        headInsertion: '',
+    ]
+
+    void setTitle(String inTitle) {
+        substrMap.title = inTitle
+        substrMap.headerInsertion = "<header>\n  <h1>$inTitle</h1>\n</header>\n"
+    }
+
+    static private String SYNTAX_MSG = 
+      $/flexmark-driver file/path.md 'The Title'/$
+
+    static void main(String[] sa) {
+        if (sa.length != 2) {
+            System.err.println SYNTAX_MSG
+            System.exit 2
+        }
+        File f = new File(sa[0])
+        if (!f.isFile()) {
+            System.err.println "No such file: ${sa[0]}"
+            System.exit 2
+        }
+        println new Gen(sa[0]).html(f.text)
+        /*
+        Gen gen = new Gen('Fancy Titlage')
+        println gen.cf(sampleMd)
+        */
+    }
+
+    /**
+     * Compares the provided ParserEmulationProfiles
+     */
+    String cf(String mdText) {
+        if (!substrMap.title)
+            throw new Error('Have not called setTitle for this Gen object')
+        (boilerplatePre.make(substrMap).toString()
+          + '\n<h1>Default? Common</h1>'
+          + BasicSample.commonMark(mdText)
+          + '\n</section>\n<section>\n<h1>Explicit Common</h1>'
+          + BasicSample.explicitCommon(mdText)
+          + '\n</section>\n<section>\n<h1>Kramdown</h1>'
+          + BasicSample.kramdown(mdText)
+          + '\n</section>\n<section>\n<h1>Multi</h1>'
+          + BasicSample.multiMarkdown(mdText)
+          + '\n</section>\n<section>\n<h1>MD</h1>'
+          + BasicSample.markdown(mdText)
+          + '\n</section>\n<section>\n<h1>Pegdown</h1>'
+          + BasicSample.pegdown(mdText)
+          + '\n</section>\n<section>\n<h1>GFM</h1>'
+          + BasicSample.gfm(mdText)
+          + '\n' + boilerplatePost)
+    }
+
+    String html(String mdText) {
+        if (!substrMap.title)
+            throw new Error('Have not called setTitle for this Gen object')
+        (boilerplatePre.make(substrMap).toString()
+          + BasicSample.explicitCommon(mdText) + '\n' + boilerplatePost)
+    }
+
+    Gen(String inTitle) throws IOException {
+        InputStream iStream
+
+        iStream = Thread.currentThread().
+          getContextClassLoader().getResourceAsStream(
+          DEFAULT_BPTOP_RES_PATH )
+        if (!iStream)
+            throw new IOException(
+              "Boilerplate inaccessible: $DEFAULT_BPTOP_RES_PATH ")
+        boilerplatePre = new SimpleTemplateEngine().
+          createTemplate(iStream.text)
+        iStream = Thread.currentThread().
+          getContextClassLoader().getResourceAsStream(
+          DEFAULT_BPBOTTOM_RES_PATH )
+        if (!iStream)
+            throw new IOException(
+              "Boilerplate inaccessible: $DEFAULT_BPBOTTOM_RES_PATH ")
+        boilerplatePost = iStream.text
+        setTitle(inTitle)
+    }
+}
---
+++ src/main/groovy/com/vladsch/flexmark/samples/BasicSample.groovy
@@ -0,0 +1,112 @@
+package com.vladsch.flexmark.samples
+
+import com.vladsch.flexmark.ast.Node
+import com.vladsch.flexmark.ext.abbreviation.AbbreviationExtension
+import com.vladsch.flexmark.ext.definition.DefinitionExtension
+import com.vladsch.flexmark.ext.footnotes.FootnoteExtension
+import com.vladsch.flexmark.ext.tables.TablesExtension
+import com.vladsch.flexmark.ext.typographic.TypographicExtension
+import com.vladsch.flexmark.ext.toc.TocExtension
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.parser.ParserEmulationProfile
+import com.vladsch.flexmark.util.options.MutableDataSet
+
+/**
+ * Groovified from https://github.com/vsch/flexmark-java/blob/master/flexmark-java-samples/src/com/vladsch/flexmark/samples/BasicSample.java
+ */
+class BasicSample {
+    static private List optList = [
+        AbbreviationExtension.create(),
+        DefinitionExtension.create(),
+        FootnoteExtension.create(),
+        TablesExtension.create(),
+        TypographicExtension.create(),
+        TocExtension.create()
+    ]
+
+    static String commonMark(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.set Parser.EXTENSIONS, optList
+        Parser parser = Parser.builder().build()
+        Node document = parser.parse inS
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+        renderer.render document
+    }
+
+    static String kramdown(String inS) {
+        /* For unknown reason, the sample config for only Kramdown
+         * sets some extension options */
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.KRAMDOWN
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+
+    static String multiMarkdown(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.MULTI_MARKDOWN
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+
+    static String markdown(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.MARKDOWN
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+
+    // Blaine adds these other variants
+
+    static String explicitCommon(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.COMMONMARK
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+
+    static String pegdown(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.PEGDOWN
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+
+    static String gfm(String inS) {
+        MutableDataSet options = new MutableDataSet()
+        options.setFrom ParserEmulationProfile.GITHUB_DOC
+        options.set Parser.EXTENSIONS, optList
+
+        Parser parser = Parser.builder(options).build()
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build()
+
+        Node document = parser.parse inS
+        renderer.render document
+    }
+}
